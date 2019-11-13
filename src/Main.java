965948decb090e2679d2df4c56e7eccf83334202
Veronika Belostotskaya <nika.nikabel@outlook.com>
2019-11-13 19:37:58 +0300

    Vector2D.2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Vector2D vA = new Vector2D();
+        Vector2D vB = new Vector2D(1.0, 3.0);
+        Vector2D vC = new Vector2D(vB);
+        vA.add(vB);
+        vA.print();
+        vA.sub(vC);
+        vA.print();
+        vB.length();
+        vC.scale(2);
+        vC.print();
+    }
+}
+
---
+++ src/Vector2D.java
@@ -0,0 +1,50 @@
+import java.util.Locale;
+
+public class Vector2D {
+    double vX, vX1,vX2;
+    double vY, vY1,vY2;
+    double length = 0;
+
+    public Vector2D() {
+        vX = 1;
+        vY = 1;
+    }
+
+    public Vector2D(double vX, double vY) {
+        this.vX = vX;
+        this.vY = vY;
+    }
+
+    public Vector2D(Vector2D v) {
+        this.vX = v.vX;
+        this.vY = v.vY;
+    }
+
+    public void print() {
+        System.out.println("(" + String.format(Locale.US, "%.2f, %.2f", vX, vY) + ")");
+
+    }
+
+    public void length() {
+        this.length = Math.sqrt(vX * vX + vY * vY);
+        System.out.println(length);
+    }
+
+    public void add(Vector2D v) {
+        this.vX = this.vX + v.vX;
+        this.vY = this.vY + v.vY;
+    }
+
+    public void sub(Vector2D v) {
+        this.vX = this.vX - v.vX;
+        this.vY = this.vY - v.vY;
+    }
+
+    public void scale(double scaleFactor) {
+        this.vX = vX*scaleFactor;
+        this.vY = vY*scaleFactor;
+    }
+
+}
+
+
